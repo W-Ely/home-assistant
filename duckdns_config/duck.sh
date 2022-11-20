35906da8781bc4e0ac491de380a23ba38191a1a6
W Ely Paysinger <paysinger@gmail.com>
2022-11-20 10:27:42 -0800

    fix duckdns behind vpn

---
+++ duckdns_config/duck.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bash
+
+. /app/duck.conf
+RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=${WANIP}")
+if [ "${RESPONSE}" = "OK" ]; then
+    echo "Your IP was updated at $(date)"
+else
+    echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
+fi
